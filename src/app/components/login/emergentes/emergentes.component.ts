562bd4f3716b096cdbb5973097a500b5ef88c20a
Carlos Carretero <carloscn90@gmail.com>
2025-01-14 14:06:18 +0100

    ahora se pueden eliminar usuarios gráficamente

--- src/app/components/login/emergentes/emergentes.component.ts
+++ src/app/components/login/emergentes/emergentes.component.ts
@@ -17,19 +17,23 @@ export class EmergentesComponent {
 
   clavesLocalStorage: string[] = [];
 
-/*   nombre: FormGroup = new FormGroup({'nombre': new FormControl('')}); */
-/* nombre: string = ''; */
+  /*   nombre: FormGroup = new FormGroup({'nombre': new FormControl('')}); */
+  /* nombre: string = ''; */
 
-  guardarDatos() {/* 
+  guardarDatos() {
+    /* 
 this.datosService.usuarioActual = this.datosService.usuarioActual.value.name; */
-const nombre: string = this.datosService.usuarioActual.value.name;
+    const nombre: string = this.datosService.usuarioActual.value.name;
 
     if (this.datosService.usuarioActual.value.name != '') {
       const datos = { name: nombre, meals: this.datosService.comidas };
       // Convertir el objeto a una cadena JSON antes de guardar
-      localStorage.setItem(this.datosService.usuarioActual.value.name, JSON.stringify(datos));
+      localStorage.setItem(
+        this.datosService.usuarioActual.value.name,
+        JSON.stringify(datos)
+      );
       alert('Datos guardados correctamente');
-      this.datosService.tipoDeSesion = 'local'
+      this.datosService.tipoDeSesion = 'local';
       this.datosService.emergenteMostrado = 'nada';
       this.datosService.subEmergenteMostrado = 'nada';
     } else {
@@ -39,7 +43,7 @@ const nombre: string = this.datosService.usuarioActual.value.name;
   cargarDatos(usuario: string) {
     const datos = localStorage.getItem(usuario);
 
-    for(var i = 0; i < this.datosService.comidas.length; i++) {
+    for (var i = 0; i < this.datosService.comidas.length; i++) {
       this.datosService.comidas[i].ignorar = true;
     }
     if (datos) {
@@ -47,19 +51,38 @@ const nombre: string = this.datosService.usuarioActual.value.name;
       const datosParseados = JSON.parse(datos);
       this.datosService.usuarioActual.value.name = datosParseados.name;
       this.datosService.comidas = datosParseados.meals;
-    /*   this.datosService.platos = [...this.datosService.platos]; */
+      /*   this.datosService.platos = [...this.datosService.platos]; */
       this.datosService.tipoDeSesion = 'local';
       this.datosService.emergenteMostrado = 'nada';
       this.datosService.subEmergenteMostrado = 'nada';
       alert('Datos cargados correctamente');
     } else {
       alert('No hay datos guardados');
-    }    
+    }
   }
+
+  borrarDatos(usuario: string) {
+   const confirmacion = confirm('¿Estás seguro de que deseas eliminar los datos?');
+
+    if (confirmacion) {
+      localStorage.removeItem(usuario);
+      alert('Datos eliminados correctamente');
+      this.datosService.tipoDeSesion = 'nada';
+    }
+  }
+
+  cerrarSesion() {
+    this.datosService.usuarioActual.value.name = '';
+    this.datosService.tipoDeSesion = 'nada';
+    this.datosService.emergenteMostrado = 'nada';
+    this.datosService.subEmergenteMostrado = 'nada';  
+  }
+
   obtenerClavesLocalStorage() {
     for (let i = 0; i < localStorage.length; i++) {
       const clave = localStorage.key(i);
-      if (clave !== null) { // Verifica si la clave no es nula (puede ocurrir en algunos casos raros)
+      if (clave !== null) {
+        // Verifica si la clave no es nula (puede ocurrir en algunos casos raros)
         this.clavesLocalStorage.push(clave);
       }
     }
